4ac9742ad8fd647127ca94fcb701e2d1bf73d09d
manil77 <manil.maharjan07@gmail.com>
2024-12-27 08:16:29 +0545

    first commit

---
+++ src/messages/messages.controller.ts
@@ -0,0 +1,13 @@
+import { Controller, Post, Get } from '@nestjs/common';
+
+@Controller('messages')
+export class MessagesController {
+  @Get()
+  listMessages() {}
+
+  @Post()
+  createMessage() {}
+
+  @Get('/:id')
+  getMessage() {}
+}
